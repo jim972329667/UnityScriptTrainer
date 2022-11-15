ebfcab7e6b773d1c94cb07574e082a74bb183e04
jim972329667 <972329667@qq.com>
2022-11-15 16:49:57 +0800

    镇邪修改器

---
+++ ZhenXie/README.md
@@ -0,0 +1,22 @@
+# 镇邪 Zhenxie 内置修改器 
+
+### 安装方法
+- 先安装 [BepInEx](https://github.com/BepInEx/BepInEx/releases/tag/v5.4.5 "BepInEx")
+- 然后将ScriptTrainer.dll放到 镇邪 Zhenxie\ZX30\BepInEx\plugins中；
+- 如果没有“plugins”文件夹自己创建一个
+- 进游戏按F9呼出修改器菜单
+
+### 功能
+- 回满血量，蓝量
+- 无敌
+- 技能点修改
+- 获取道具
+
+### 特别鸣谢
+
+* [3DM Mod组](https://github.com/GlossMod)
+
+### 更新日志
+
+v0.01
+- 初始发布
---
+++ ZhenXie/ScriptTrainer/Script.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ScriptTrainer
+{
+    class Script: MonoBehaviour
+    {
+        /// <summary>
+        /// 检查是否是int
+        /// </summary>
+        /// <param name="ItemText"></param>
+        public static int CheckIsInt(string ItemText)
+        {
+            int newCount = 0;
+            ItemText = Regex.Replace(ItemText, @"[\-?][^0-9.]", "");
+            try
+            {
+                if (ItemText != null && ItemText.Length < 10 && ItemText.Length != 0)
+                {
+                    newCount = Int32.Parse(ItemText);
+                }
+                else
+                {
+                    ItemText = newCount.ToString();
+                }
+            }
+            catch (Exception) { throw; }
+
+            return newCount;
+        }
+
+        public static float CheckIsFloat(string ItemText)
+        {
+            float newFloat = 0f;
+
+            try
+            {
+                if (ItemText != null && ItemText.Length < 10 && ItemText.Length != 0)
+                {
+                    float.TryParse(ItemText, out float result);
+
+                    newFloat = result;
+                }
+                else
+                {
+                    ItemText = newFloat.ToString();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return newFloat;
+        }
+    }
+}
